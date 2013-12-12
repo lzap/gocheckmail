f1efffee1b2081ec4ef1361f6b495d80b5a422f8
Lukas Zapletal <lzap+git@redhat.com>
2013-12-12 17:09:05 +0100

    New version

--- gocheckmail.go
+++ gocheckmail.go
@@ -1,49 +1,78 @@
 package main
 
 import (
-	"code.google.com/p/goconf/conf"
-	"github.com/howeyc/fsnotify"
 	"log"
 	"os"
 	"os/exec"
+	"time"
+	"bufio"
+	"regexp"
+	//"io"
+	"path"
 )
 
+var subject_regexp = regexp.MustCompile(`^Subject:\s*(.*)$`)
+
+func report(msg string) {
+	cmd := exec.Command("/bin/bash", "-c", "/usr/bin/notify-send '" + msg + "'")
+	err := cmd.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func read_subject(filename string) string {
+		// check for new mail
+		d, err := os.Open(filename)
+		if err != nil {
+			log.Fatal(err)
+			return "?"
+		}
+		defer d.Close()
+		//scanner := bufio.NewScanner(io.LimitReader(d, 5000))
+		scanner := bufio.NewScanner(d)
+		for scanner.Scan() {
+			if subject_regexp.Match([]byte(scanner.Text())) {
+				return subject_regexp.FindStringSubmatch(scanner.Text())[0]
+			}
+		}
+		return "?"
+}
+
 func main() {
-	var path = os.ExpandEnv("$HOME/mail")
-	var command = os.ExpandEnv("/usr/bin/notify-send 'New mail'")
-	c, err := conf.ReadConfigFile(os.ExpandEnv("$HOME/.gocheckmail.conf"))
-	if err == nil {
-		path, _ = c.GetString("default", "path")
-		command, _ = c.GetString("default", "command")
-		path = os.ExpandEnv(path)
-	}
-
-	watcher, err := fsnotify.NewWatcher()
-	if err != nil {
-		log.Fatalf("error creating inotify watcher: %s", err)
-	}
-
-	err = watcher.Watch(path)
-	if err != nil {
-		log.Fatalf("error while opening %s: %s", path, err)
-	}
-	defer func() {
-		watcher.Close()
-	}()
+	var dirname = os.ExpandEnv("$HOME/Mail/INBOX")
+	var latest = time.Now()
 
 	for {
-		select {
-		case ev := <-watcher.Event:
-			//log.Print(ev)
-			if ev.IsCreate() {
-				cmd := exec.Command("/bin/bash", "-c", command)
-				err := cmd.Start()
-				if err != nil {
-					log.Fatal(err)
+		time.Sleep(4 * time.Minute)
+
+		// check for new mail
+		d, err := os.Open(dirname)
+		if err != nil {
+			log.Fatal(err)
+			os.Exit(1)
+		}
+		defer d.Close()
+		fi, err := d.Readdir(-1)
+		if err != nil {
+			log.Fatal(err)
+			os.Exit(1)
+		}
+		for _, fi := range fi {
+			if fi.Mode().IsRegular() {
+				if fi.ModTime().After(latest) {
+					report("New mail: " + read_subject(path.Join(dirname, fi.Name())))
+					latest = fi.ModTime()
+					// no rush with opening bubbles
+					time.Sleep(5 * time.Second)
 				}
 			}
-		case err := <-watcher.Error:
-			log.Printf("error event from inotify: %s", err)
+		}
+
+		// report hours every hour (break time!)
+		if time.Now().Minute() >= 55 {
+			report(time.Now().Format("15:04"))
+			time.Sleep(64 * time.Second)
 		}
 	}
 }
